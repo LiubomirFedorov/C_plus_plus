253e86b11aad6ce77f044f03b3f34a22e319414c
LiubomirFedorov <114654628+LiubomirFedorov@users.noreply.github.com>
2023-10-24 22:42:51 +0300

    Add files via upload

---
+++ practic6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter a positive integer N: ";
+    cin >> N;
+
+    int sum = 0;
+
+    while (N > 0) {
+        int digit = N % 10; 
+
+        if (digit < 7) {
+            sum += digit;
+        }
+
+        N /= 10; 
+    }
+
+    cout << "The sum of digits less than 7 in a number: " << sum << endl;
+
+    return 0;
+}
